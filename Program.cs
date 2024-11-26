7ae446158abe5b9fa601d638a1573f59faf1eb84
John DeVries <johndevries49@gmail.com>
2024-11-26 11:43:47 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Challenge4_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> count = new Dictionary<string, int>();
+
+            string[] input = ["a", "b", "c", "a", "c", "d","f","d"];
+
+            foreach (string s in input)
+            {
+                if (!count.TryAdd(s, 1))
+                {
+                    count[s]++;
+                }
+            }
+
+            //Print dictionary
+            Console.WriteLine();
+            foreach (KeyValuePair<string, int> kvp in count)
+            {
+                Console.WriteLine("Key = {0}, Count = {1}",
+                    kvp.Key, kvp.Value);
+            }
+
+
+        }
+    }
+}
